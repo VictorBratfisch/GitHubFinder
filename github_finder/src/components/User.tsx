df6a2e2241f6704775ba33d361b6bfb0b3a8cc19
VictorBratfisch <victorbratfisch22@gmail.com>
2023-08-21 10:53:14 -0300

    github finder

---
+++ github_finder/src/components/User.tsx
@@ -0,0 +1,42 @@
+import { UserProps } from "../types/user";
+
+import { MdLocationPin } from "react-icons/md";
+
+import { Link } from "react-router-dom";
+
+import classes from "./User.module.css";
+
+const User = ({
+  avatar_url,
+  login,
+  location,
+  followers,
+  following,
+}: UserProps) => {
+  return (
+    <div className={classes.user}>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      {location && (
+        <p className={classes.location}>
+          <MdLocationPin />
+          <span>{location}</span>
+        </p>
+      )}
+
+      <div className={classes.stats}>
+        <div>
+          <p>Seguidores:</p>
+          <p className={classes.number}>{followers}</p>
+        </div>
+        <div>
+          <p>Seguindo:</p>
+          <p className={classes.number}>{following}</p>
+        </div>
+      </div>
+      {/* <Link to={`/repos/${login}`}>Ver melhores projetos</Link> */}
+    </div>
+  );
+};
+
+export default User;
